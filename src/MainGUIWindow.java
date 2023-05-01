05df62276dcfc0eb7019d78ec8aa4797ab72a682
Alexandre Arturi <alexandrea8@nycstudents.net>
2023-05-01 11:36:44 -0400

    5-1

--- src/MainGUIWindow.java
+++ src/MainGUIWindow.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class MainGUIWindow {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MainGUIWindow extends JFrame implements ActionListener, KeyListener {
+    private JPanel mainPanel;
+    private JTextArea textArea1;
+    private JButton areGUISAwesomeButton;
+
+    public MainGUIWindow(){
+        createUIComponents();
+    }
+
+    private void createUIComponents(){
+        setContentPane(mainPanel);
+        setTitle("My GUI!");
+        setSize(300,400);
+        setLocation(450,100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        areGUISAwesomeButton.addActionListener(this);
+        textArea1.addKeyListener(this);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        Object source = e.getSource();
+        if (source instanceof JButton){
+            JButton button = (JButton) source;
+            String text = button.getText();
+
+            if(text.equals("Are GUI's awesome?")){
+                textArea1.append("Button clicked");
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+
+    }
 }
